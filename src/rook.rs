374fcbb84a1235ee5d51ce680b3ad1d7c5d46ee9
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-11-30 01:48:53 +0100

    Add rook

---
+++ src/rook.rs
@@ -0,0 +1,52 @@
+use crate::bitboard::Bitboard;
+use crate::ray::Ray;
+
+pub struct Rook;
+
+impl Rook {
+    fn targets(origin: usize, occupied: Bitboard) -> Bitboard {
+        Ray::north_targets(origin, occupied)
+            | Ray::east_targets(origin, occupied)
+            | Ray::south_targets(origin, occupied)
+            | Ray::west_targets(origin, occupied)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rank_and_file_targets() {
+        assert_eq!(
+            Bitboard::C4
+                | Bitboard::D3
+                | Bitboard::D2
+                | Bitboard::D5
+                | Bitboard::D6
+                | Bitboard::E4
+                | Bitboard::F4
+                | Bitboard::G4,
+            Rook::targets(
+                Bitboard::IDX_D4,
+                Bitboard::C4
+                    | Bitboard::B4
+                    | Bitboard::A4
+                    | Bitboard::D2
+                    | Bitboard::D6
+                    | Bitboard::G4
+            )
+        );
+    }
+
+    #[test]
+    fn non_blocking_occupancy_targets() {
+        assert_eq!(
+            Rook::targets(Bitboard::IDX_D4, Bitboard::EMPTY),
+            Rook::targets(
+                Bitboard::IDX_D4,
+                Bitboard::C3 | Bitboard::C5 | Bitboard::E3 | Bitboard::E5
+            )
+        );
+    }
+}
