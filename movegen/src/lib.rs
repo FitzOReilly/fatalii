801e18e29bafec79f7ba5cbcf2d093a1154b64d8
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2021-03-07 13:30:57 +0100

    Implement simple evaluation function
    - Calculate score with material and mobility

--- movegen/src/lib.rs
+++ movegen/src/lib.rs
@@ -1,23 +1,23 @@
 #[macro_use]
 extern crate bitflags;
 
+pub mod bishop;
+pub mod bitboard;
 pub mod fen;
+pub mod king;
+pub mod knight;
 pub mod r#move;
 pub mod move_generator;
+pub mod pawn;
+pub mod piece;
 pub mod position;
 pub mod position_history;
+pub mod queen;
+pub mod rook;
+pub mod side;
+pub mod square;
 
-mod bishop;
-mod bitboard;
 mod direction;
 mod file;
-mod king;
-mod knight;
-mod pawn;
-mod piece;
-mod queen;
 mod rank;
 mod ray;
-mod rook;
-mod side;
-mod square;
