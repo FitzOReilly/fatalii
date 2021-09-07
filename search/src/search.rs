9aa61fab006993b282c02a186c8631cb36522780
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2021-09-07 14:27:45 +0200

    Add depth to SearchResult and implement Display trait

--- search/src/search.rs
+++ search/src/search.rs
@@ -1,17 +1,28 @@
 use eval::eval::Score;
 use movegen::position_history::PositionHistory;
 use movegen::r#move::Move;
+use std::fmt;
 use std::ops::Neg;
 
 #[derive(Debug, PartialEq)]
 pub struct SearchResult {
+    depth: u8,
     score: Score,
     best_move: Move,
 }
 
 impl SearchResult {
-    pub fn new(score: Score, best_move: Move) -> SearchResult {
-        SearchResult { score, best_move }
+    pub fn new(depth: usize, score: Score, best_move: Move) -> SearchResult {
+        debug_assert!(depth < 256);
+        SearchResult {
+            depth: depth as u8,
+            score,
+            best_move,
+        }
+    }
+
+    fn depth(&self) -> usize {
+        self.depth as usize
     }
 
     pub fn score(&self) -> Score {
@@ -28,7 +39,19 @@ impl Neg for SearchResult {
 
     // Changes the sign of the score and leaves the best move unchanged
     fn neg(self) -> Self::Output {
-        Self::new(-self.score(), self.best_move())
+        Self::new(self.depth(), -self.score(), self.best_move())
+    }
+}
+
+impl fmt::Display for SearchResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "depth: {}, score: {}, best move: {}",
+            self.depth(),
+            self.score(),
+            self.best_move()
+        )
     }
 }
 
