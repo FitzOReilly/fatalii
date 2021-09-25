6e7bd26c36709db0cbb6f3df588962de1bc1cff4
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2021-09-25 23:18:51 +0200

    Implement UCI parser

---
+++ uci/tests/uci.rs
@@ -0,0 +1,36 @@
+use engine::Engine;
+use search::alpha_beta::AlphaBeta;
+use std::io::Write;
+use std::str;
+use uci::parser::Parser;
+
+const TABLE_IDX_BITS: usize = 16;
+
+#[test]
+fn register_command() {
+    let search_algo = AlphaBeta::new(TABLE_IDX_BITS);
+    let mut engine = Engine::new(search_algo);
+    let mut test_writer = Vec::new();
+    let mut p = Parser::new(&mut test_writer);
+
+    assert!(p.run_command("unknown\n", &mut engine).is_err());
+    assert!(p.run_command("cmd\n", &mut engine).is_err());
+    assert!(p.run_command("cmd args\n", &mut engine).is_err());
+    assert!(p.run_command(" \r \t  cmd  args \n", &mut engine).is_err());
+
+    let cmd_handler = |writer: &mut dyn Write, args: &str, _engine: &mut Engine| {
+        Ok(writeln!(writer, "{}", args.trim_end_matches('\n'))?)
+    };
+    p.register_command(String::from("cmd"), Box::new(cmd_handler));
+
+    assert!(p.run_command("unknown\n", &mut engine).is_err());
+    assert!(p.run_command("cmd\n", &mut engine).is_ok());
+    assert!(p.run_command("cmd args\n", &mut engine).is_ok());
+    assert!(p.run_command(" \r \t  cmd  args \n", &mut engine).is_ok());
+
+    let mut outputs = str::from_utf8(&test_writer).unwrap().split('\n');
+
+    assert_eq!("", outputs.next().unwrap());
+    assert_eq!("args", outputs.next().unwrap());
+    assert_eq!(" args ", outputs.next().unwrap());
+}
