392f6fa483431df40457383d688862632274723b
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2022-02-18 19:38:50 +0100

    Count nodes during search
    - Moves made
    - Transposition table hits
    - Evaluate calls
    - Add nodes (moves made) to UCI info output

---
+++ search/src/node_counter.rs
@@ -0,0 +1,73 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct NodeCounter {
+    node_counts: Vec<Vec<(u64, u64)>>,
+    eval_count: Vec<u64>,
+    max_depth: usize,
+}
+
+impl NodeCounter {
+    pub fn new() -> Self {
+        Self {
+            node_counts: Vec::new(),
+            eval_count: Vec::new(),
+            max_depth: 0,
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.node_counts.clear();
+        self.eval_count.clear();
+        self.max_depth = 0;
+    }
+
+    pub fn increment_nodes(&mut self, search_depth: usize, plies_from_end: usize) {
+        self.reserve(search_depth);
+        self.node_counts[search_depth - 1][plies_from_end].0 += 1;
+    }
+
+    pub fn increment_cache_hits(&mut self, search_depth: usize, plies_from_end: usize) {
+        self.reserve(search_depth);
+        self.node_counts[search_depth - 1][plies_from_end].1 += 1;
+    }
+
+    pub fn increment_eval_calls(&mut self, search_depth: usize) {
+        self.reserve(search_depth);
+        self.eval_count[search_depth - 1] += 1;
+    }
+
+    pub fn sum_nodes(&self) -> u64 {
+        self.node_counts
+            .iter()
+            .map(|nc| nc.iter().map(|x| x.0).sum::<u64>())
+            .sum()
+    }
+
+    fn reserve(&mut self, search_depth: usize) {
+        if search_depth > self.max_depth {
+            debug_assert!(search_depth == self.max_depth + 1);
+            self.node_counts.push(vec![(0, 0); search_depth + 1]);
+            self.eval_count.push(0);
+            self.max_depth += 1;
+        }
+    }
+}
+
+impl fmt::Display for NodeCounter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for d in 1..=self.max_depth {
+            writeln!(f, "Search depth {}:", d)?;
+            writeln!(f, "\tEvaluate calls: {}", self.eval_count[d - 1])?;
+            for p in 0..=d {
+                let nc = &self.node_counts[d - 1][p];
+                writeln!(
+                    f,
+                    "\tPlies from end of PV / moves made / cache hits: {} / {} / {}",
+                    p, nc.0, nc.1,
+                )?;
+            }
+        }
+        Ok(())
+    }
+}
--- search/tests/search.rs
+++ search/tests/search.rs
@@ -106,6 +106,7 @@ fn checkmate_white(search_algo: impl Search + Send + 'static) {
     let expected = SearchResult::new(
         depth,
         CHECKMATE_WHITE,
+        0,
         Move::new(Square::D8, Square::H4, MoveType::QUIET),
         MoveList::new(),
     );
@@ -131,6 +132,7 @@ fn checkmate_black(search_algo: impl Search + Send + 'static) {
     let expected = SearchResult::new(
         depth,
         CHECKMATE_BLACK,
+        0,
         Move::new(Square::A1, Square::A8, MoveType::QUIET),
         MoveList::new(),
     );
@@ -151,12 +153,13 @@ fn stalemate(search_algo: impl Search + Send + 'static) {
     let pos_history = PositionHistory::new(pos);
 
     let depth = 1;
-    let expected = SearchResult::new(depth, EQUAL_POSITION, Move::NULL, MoveList::new());
+    let expected = SearchResult::new(depth, EQUAL_POSITION, 0, Move::NULL, MoveList::new());
 
     let mut tester = SearchTester::new(search_algo);
     let actual = tester.search(pos_history, depth);
     assert_eq!(expected.depth(), actual.depth());
     assert_eq!(expected.score(), actual.score());
+    assert_eq!(expected.nodes(), actual.nodes());
     assert_eq!(expected.best_move(), actual.best_move());
 }
 
