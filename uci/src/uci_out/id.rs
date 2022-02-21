bd8eda78ec18838e908d2efeeee9d5a5f0272483
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2022-02-21 19:17:34 +0100

    Increase version to 0.2.0 and add it to engine id

--- uci/src/uci_out/id.rs
+++ uci/src/uci_out/id.rs
@@ -1,6 +1,12 @@
 use std::error::Error;
 use std::io::Write;
 
+pub static mut ENGINE_VERSION: &str = "";
+
 pub fn write(writer: &mut impl Write) -> Result<(), Box<dyn Error>> {
-    Ok(write!(writer, "id name Fatalii\nid author Patrick Heck\n")?)
+    Ok(write!(
+        writer,
+        "id name Fatalii {}\nid author Patrick Heck\n",
+        unsafe { ENGINE_VERSION }
+    )?)
 }
