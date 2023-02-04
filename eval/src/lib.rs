66a5894d46643402740dac936bcb806c16ec2a8b
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2023-02-04 16:21:47 +0100

    Give the side to move a small evaluation bonus

--- eval/src/lib.rs
+++ eval/src/lib.rs
@@ -2,6 +2,7 @@ pub use crate::eval::{
     Eval, Score, CHECKMATE_BLACK, CHECKMATE_WHITE, EQUAL_POSITION, NEGATIVE_INF, POSITIVE_INF,
 };
 
+pub mod complex;
 pub mod eval;
 pub mod material_mobility;
 pub mod piece_square_tables;
