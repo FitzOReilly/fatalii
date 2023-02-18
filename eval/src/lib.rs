de0cd9be65ceef67ff61e2bf7a47e2192be0201b
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2023-02-18 14:14:41 +0100

    Remove (mostly) duplicate code

--- eval/src/lib.rs
+++ eval/src/lib.rs
@@ -4,5 +4,4 @@ pub use crate::score::{Score, ScoreVariant, BLACK_WIN, EQ_POSITION, NEG_INF, POS
 pub mod complex;
 pub mod eval;
 pub mod material_mobility;
-pub mod piece_square_tables;
 pub mod score;
