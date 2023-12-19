6ce38710144c592f8b7e834143d860393fc569f0
Patrick Heck <49785565+FitzOReilly@users.noreply.github.com>
2023-12-19 15:35:30 +0100

    Refactor MoveCandidates

--- search/src/move_candidates.rs
+++ search/src/move_candidates.rs
@@ -30,21 +30,13 @@ impl From<&MoveList> for MoveCandidates {
 
 impl MoveCandidates {
     pub fn move_to_front(&mut self, best_move: Move) {
-        let idx = self
-            .move_list
-            .iter()
-            .position(|x| x.r#move == best_move)
-            .expect("Expected to find move {x} in candidates: {candidates:?}");
+        let idx = self.index(best_move);
         let slice = &mut self.move_list[0..=idx];
         slice.rotate_right(1);
     }
 
     pub fn set_subtree_size(&mut self, m: Move, node_count: u64) {
-        let idx = self
-            .move_list
-            .iter()
-            .position(|x| x.r#move == m)
-            .expect("Expected to find move {x} in candidates: {candidates:?}");
+        let idx = self.index(m);
         self.move_list[idx].subtree_size = node_count;
     }
 
@@ -56,5 +48,20 @@ impl MoveCandidates {
     pub fn reset_counts(&mut self) {
         self.current_idx = 0;
         self.alpha_raised_count = 0;
+    }
+
+    fn index(&self, m: Move) -> usize {
+        self.move_list
+            .iter()
+            .position(|x| x.r#move == m)
+            .unwrap_or_else(|| {
+                panic!(
+                    "Expected to find move {m} in candidates: {:?}",
+                    self.move_list
+                        .iter()
+                        .map(|x| format!("{}", x.r#move))
+                        .collect::<Vec<_>>(),
+                )
+            })
     }
 }
