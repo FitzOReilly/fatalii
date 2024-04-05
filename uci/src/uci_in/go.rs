464620cdda007daf0cd3b07449454133bfffd057
Patrick Heck <49785565+FitzOReilly@users.noreply.github.com>
2024-04-05 17:54:37 +0200

    Replace negative values with 0 in uci go command

--- uci/src/uci_in/go.rs
+++ uci/src/uci_in/go.rs
@@ -9,39 +9,43 @@ use std::error::Error;
 use std::time::Duration;
 
 pub fn run_command(
-    _uci_out: &mut UciOut,
+    uci_out: &mut UciOut,
     args: &str,
     engine: &mut Engine,
 ) -> Result<Option<ParserMessage>, Box<dyn Error>> {
-    let options = parse_options(args, engine)?;
+    let options = parse_options(uci_out, args, engine)?;
     run(options, engine)
 }
 
-fn parse_options(args: &str, engine: &Engine) -> Result<SearchOptions, Box<dyn Error>> {
+fn parse_options(
+    uci_out: &UciOut,
+    go_args: &str,
+    engine: &Engine,
+) -> Result<SearchOptions, Box<dyn Error>> {
     let mut options = SearchOptions::default();
     let mut seen_options = HashSet::new();
-    let mut s = args;
-    while let Some((cmd, tail)) = split_first_word(s) {
-        if !seen_options.insert(cmd) {
+    let mut s = go_args;
+    while let Some((opt, tail)) = split_first_word(s) {
+        if !seen_options.insert(opt) {
             return Err(Box::new(UciError::InvalidArgument(format!(
-                "Option `{cmd}` must not appear more than once in\ngo {args}",
+                "Option `{opt}` must not appear more than once in\ngo {go_args}",
             ))));
         }
-        s = match cmd {
+        s = match opt {
             "searchmoves" => parse_moves(tail, &mut options.search_moves, engine)?,
             "ponder" => {
                 options.ponder = true;
                 tail
             }
-            "wtime" => parse_duration(tail, &mut options.white_time)?,
-            "btime" => parse_duration(tail, &mut options.black_time)?,
-            "winc" => parse_duration(tail, &mut options.white_inc)?,
-            "binc" => parse_duration(tail, &mut options.black_inc)?,
-            "movestogo" => parse_usize(tail, &mut options.moves_to_go)?,
-            "depth" => parse_usize(tail, &mut options.depth)?,
-            "nodes" => parse_usize(tail, &mut options.nodes)?,
-            "mate" => parse_usize(tail, &mut options.mate_in)?,
-            "movetime" => parse_duration(tail, &mut options.movetime)?,
+            name @ "wtime" => parse_duration(uci_out, name, tail, &mut options.white_time)?,
+            name @ "btime" => parse_duration(uci_out, name, tail, &mut options.black_time)?,
+            name @ "winc" => parse_duration(uci_out, name, tail, &mut options.white_inc)?,
+            name @ "binc" => parse_duration(uci_out, name, tail, &mut options.black_inc)?,
+            name @ "movestogo" => parse_usize(uci_out, name, tail, &mut options.moves_to_go)?,
+            name @ "depth" => parse_usize(uci_out, name, tail, &mut options.depth)?,
+            name @ "nodes" => parse_usize(uci_out, name, tail, &mut options.nodes)?,
+            name @ "mate" => parse_usize(uci_out, name, tail, &mut options.mate_in)?,
+            name @ "movetime" => parse_duration(uci_out, name, tail, &mut options.movetime)?,
             "infinite" => {
                 options.infinite = true;
                 tail
@@ -49,7 +53,7 @@ fn parse_options(args: &str, engine: &Engine) -> Result<SearchOptions, Box<dyn E
             _ => {
                 return Err(Box::new(UciError::InvalidArgument(format!(
                     "go {}",
-                    args.trim_end()
+                    go_args.trim_end()
                 ))))
             }
         }
@@ -57,11 +61,22 @@ fn parse_options(args: &str, engine: &Engine) -> Result<SearchOptions, Box<dyn E
     Ok(options)
 }
 
-fn parse_usize<'a>(args: &'a str, number: &mut Option<usize>) -> Result<&'a str, Box<dyn Error>> {
+fn parse_usize<'a>(
+    uci_out: &UciOut,
+    opt: &str,
+    args: &'a str,
+    number: &mut Option<usize>,
+) -> Result<&'a str, Box<dyn Error>> {
     debug_assert!(number.is_none());
     match split_first_word(args) {
         Some((first_word, tail)) => {
-            *number = Some(first_word.parse::<usize>()?);
+            let signed_num = first_word.parse::<i64>()?;
+            if signed_num < 0 {
+                uci_out.warn(&format!(
+                    "ignoring negative value `{opt} {signed_num}`, using `{opt} 0` instead",
+                ))?;
+            }
+            *number = Some(signed_num.max(0) as usize);
             Ok(tail)
         }
         _ => Err(Box::new(UciError::InvalidArgument(format!("go {args}")))),
@@ -69,13 +84,21 @@ fn parse_usize<'a>(args: &'a str, number: &mut Option<usize>) -> Result<&'a str,
 }
 
 fn parse_duration<'a>(
+    uci_out: &UciOut,
+    opt: &str,
     args: &'a str,
     dur: &mut Option<Duration>,
 ) -> Result<&'a str, Box<dyn Error>> {
     debug_assert!(dur.is_none());
     match split_first_word(args) {
         Some((first_word, tail)) => {
-            *dur = Some(Duration::from_millis(first_word.parse::<u64>()?));
+            let signed_dur = first_word.parse::<i64>()?;
+            if signed_dur < 0 {
+                uci_out.warn(&format!(
+                    "ignoring negative value `{opt} {signed_dur}`, using `{opt} 0` instead",
+                ))?;
+            }
+            *dur = Some(Duration::from_millis(signed_dur.max(0) as u64));
             Ok(tail)
         }
         _ => Err(Box::new(UciError::InvalidArgument(format!("go {args}")))),
